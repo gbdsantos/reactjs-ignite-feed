8e7928f0a9750fd623d111733a071956f371400c
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-06-24 13:37:44 -0300

    :recycle: refactor(frontend): add typescript on react

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatar_url: 'https://github.com/gbdsantos.png',
+      name: 'Guilherme Bezerra',
+      role: 'Software Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal 👋' },
+      { type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻' },
+      { type: 'link', content: 'devonlane.design' },
+      { type: 'paragraph', content: 'Acesse e deixe seu feedback 👉' }
+    ],
+    publishedAt: new Date('2022-06-09 00:04:30')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatar_url: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'Educator at @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala pessoal 👋' },
+      { type: 'paragraph', content: 'Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻' },
+      { type: 'link', content: 'devonlane.design' },
+      { type: 'paragraph', content: 'Acesse e deixe seu feedback 👉' }
+    ],
+    publishedAt: new Date('2022-06-10 20:10:42')
+  }
+];
+
+function App() {
+  console.log(posts);
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {
+            posts.map(post => {
+              return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  key={post.id}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+
+          }
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Post.tsx
@@ -0,0 +1,141 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
+
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+
+import styles from './Post.module.css';
+
+interface Author {
+  avatar: string;
+  avatar_url: string;
+  name: string;
+  role: string;
+}
+
+interface PostProps {
+  author: Author;
+  content: Content[];
+  publishedAt: Date;
+}
+
+interface Content {
+  content: string;
+  type: 'paragraph' | 'link';
+}
+
+export function Post({ author, content, publishedAt }: PostProps) {
+  const [newCommentText, setNewCommentText] = useState('');
+
+  const [comments, setComments] = useState([
+    'Post muito show!',
+    2
+  ]);
+
+  const publishedDateFormatted2 = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    addSuffix: true,
+    locale: ptBR
+  })
+
+  const publishedDateFormatted = new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: 'long',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(publishedAt);
+
+  function handleCreateNewComment(event: FormEvent) {
+    event.preventDefault();
+
+    // const newCommentText = event.target.comment.value;
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity('');
+    setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity('Esse campo é obrigatório');
+  }
+
+  function deleteComment(commentToDelete: string) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete;
+    });
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatar_url} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time dateTime={publishedAt.toISOString()} title={publishedDateFormatted2}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p key={line.content}>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p key={line.content}><a href="">{line.content}</a></p>;
+          }
+        })}
+      </div>
+
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe o seu feedback</strong>
+
+        <textarea
+          name="comment"
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          placeholder="Deixe um comentário"
+          required
+          value={newCommentText}
+        />
+
+        <footer>
+          <button
+            disabled={isNewCommentEmpty}
+            type="submit"
+          >
+            Publicar
+          </button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return (
+            <Comment
+              content={comment}
+              key={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
+        })}
+      </div>
+    </article>
+  );
+}
